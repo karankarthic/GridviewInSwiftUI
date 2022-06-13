d46e0acd255c03c515dd080950c5eb2d5d435602
karankarthic <44661868+karankarthic@users.noreply.github.com>
2022-06-13 11:39:11 +0530

    Initial Commit

---
+++ GridviewInSwiftUI/GridviewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridviewInSwiftUIApp.swift
+//  GridviewInSwiftUI
+//
+//  Created by Karan Karthic Neelamegan on 13/06/22.
+//
+
+import SwiftUI
+
+@main
+struct GridviewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
